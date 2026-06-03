6e7eb9664b14e70408963178a3a791b692bae2cf
Philipp <P.Rodowski@live.de>
2026-06-03 10:51:29 +0200

    - Added toString() in DVD

--- src/main/java/de/schulung/bibliothek/media/DVD.java
+++ src/main/java/de/schulung/bibliothek/media/DVD.java
@@ -7,7 +7,7 @@ public class DVD extends Medium {
     private final String director;
     private final String length;
 
-    public DVD(int id, String title, Language language, String year, String director, String length) {
+    public DVD(int id, String title, Language language, int year, String director, String length) {
         super(id, title, language, year);
         this.director = director;
         this.length = length;
@@ -19,4 +19,18 @@ public class DVD extends Medium {
     public String getDirector() {
             return director;
         }
+
+
+    @Override
+    public String toString() {
+        return "DVD{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", director='" + director + '\'' +
+                ", language=" + language +
+                ", year=" + year +
+                ", length='" + length + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
